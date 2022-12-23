5c67ebd118e3b54d5bfd9cf323ce513f77108630
Ben Stanfield <bnstnfld@gmail.com>
2022-12-23 11:29:56 -0800

    push missing up file

--- migrations/sqls/20221223192106-add-game-slug-to-rounds-up.sql
+++ migrations/sqls/20221223192106-add-game-slug-to-rounds-up.sql
@@ -1,1 +1,7 @@
-/* Replace with your SQL commands */
+/* Replace with your SQL commands ?? */
+/* Rename column game_id to game_slug in table rounds */
+ALTER TABLE rounds RENAME COLUMN IF EXISTS game_id TO game_slug;
+/* Recast column game_slug to type text */
+ALTER TABLE rounds ALTER COLUMN game_slug TYPE text;
+/* Assign foreign key games.slug to column game_slug in table rounds */
+ALTER TABLE rounds ADD CONSTRAINT rounds_game_slug_fkey FOREIGN KEY (game_slug) REFERENCES games(slug);
