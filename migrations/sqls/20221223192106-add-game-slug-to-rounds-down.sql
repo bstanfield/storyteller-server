733cc358b00264b85b0d490a3055718f5b887b94
Mimi Tran Zambetti <mimi@projectwren.com>
2022-12-23 11:37:23 -0800

    fix migration

--- migrations/sqls/20221223192106-add-game-slug-to-rounds-down.sql
+++ migrations/sqls/20221223192106-add-game-slug-to-rounds-down.sql
@@ -1,7 +1,3 @@
-/* Replace with your SQL commands */
-/* Rename column game_slug to game_id in table rounds */
-ALTER TABLE rounds RENAME COLUMN IF EXISTS game_slug TO game_id;
-/* Recast column game_id to type integer */
-ALTER TABLE rounds ALTER COLUMN game_id TYPE integer;
-/* Assign foreign key games.id to column game_id in table rounds */
-ALTER TABLE rounds ADD CONSTRAINT rounds_game_id_fkey FOREIGN KEY (game_id) REFERENCES games(id);
+ALTER TABLE rounds DROP COLUMN IF EXISTS game_slug;
+ALTER TABLE rounds ADD COLUMN IF NOT EXISTS game_id integer;
+ALTER TABLE rounds ADD CONSTRAINT rounds_game_slug_fkey FOREIGN KEY (game_id) REFERENCES games(id);
--- migrations/sqls/20221223192106-add-game-slug-to-rounds-up.sql
+++ migrations/sqls/20221223192106-add-game-slug-to-rounds-up.sql
@@ -1,7 +1,3 @@
-/* Replace with your SQL commands ?? */
-/* Rename column game_id to game_slug in table rounds */
-ALTER TABLE rounds RENAME COLUMN IF EXISTS game_id TO game_slug;
-/* Recast column game_slug to type text */
-ALTER TABLE rounds ALTER COLUMN game_slug TYPE text;
-/* Assign foreign key games.slug to column game_slug in table rounds */
+ALTER TABLE rounds DROP COLUMN IF EXISTS game_id;
+ALTER TABLE rounds ADD COLUMN IF NOT EXISTS game_slug text;
 ALTER TABLE rounds ADD CONSTRAINT rounds_game_slug_fkey FOREIGN KEY (game_slug) REFERENCES games(slug);
